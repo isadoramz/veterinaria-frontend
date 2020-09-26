7ec2deb85ce5b0817d7369e3b95730448377aa5f
Isadora Zandonotto <isadoraimz@gmail.com>
2020-09-25 21:30:19 -0300

    altera arquivos Pesquisa, Cadastro

--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -1,47 +1,46 @@
 <template>
-  <div>
-    <div class="container">
-      <div>
+  <div class="container">
+    <div class="row">
+      <div class="col-3">
         <img class="vue-dog" alt="VueDog" src="../assets/vue-dog.jpg">
       </div>
-      <div class="cadastro-txt">
+      <div class="cadastro-txt col-4">
         <Welcome msg="Cadastro de Pet" txt="Cadastre os dados do pet aqui"></Welcome>
-      </div>
-      <div class="div-input ">
-        <form>
-            <div class="form-row">
-                <div class="col-7">
-                    <label for="nome">Nome</label>
-                    <input type="nome" class="form-control" id="nome">
-                </div>
-                <div class="col-3">
-                    <label for="tipo">Tipo</label>
-                    <select id="tipo" class="form-control">
-                        <option selected>Selecione</option>
-                        <option>Cachorro</option>
-                    </select>
-                </div>
-                <div class="col-3">
-                    <label for="raça">Raça</label>
-                    <select id="raça" class="form-control">
-                        <option selected>Selecione</option>
-                        <option>SRD</option>
-                    </select>
-                </div>
-                <div class="col-7 form-group">
-                    <label for="idade">Idade</label>
-                    <input type="idade" class="form-control" id="idade">
-                </div> 
-            </div>
-            <button type="submit" class="btn-cadastro btn">Salvar</button>
-        </form>
-      </div>
+        <div class="div-input">
+          <form>
+              <div class="form-row">
+                  <div class="col-4">
+                      <label for="nome">Nome</label>
+                      <input type="nome" class="form-control" id="nome">
+                  </div>
+                  <div class="col-4">
+                      <label for="tipo">Tipo</label>
+                      <select id="tipo" class="form-control">
+                          <option selected>Selecione</option>
+                          <option>Cachorro</option>
+                      </select>
+                  </div>
+                  <div class="col-4">
+                      <label for="raça">Raça</label>
+                      <select id="raça" class="form-control">
+                          <option selected>Selecione</option>
+                          <option>SRD</option>
+                      </select>
+                  </div>
+                  <div class="col-4 form-group">
+                      <label for="idade">Idade</label>
+                      <input type="idade" class="form-control" id="idade">
+                  </div> 
+              </div>
+              <button type="submit" class="btn-cadastro btn">Salvar</button>
+          </form>
+        </div>
+      </div>  
     </div> 
   </div>
 </template>
 
 <script>
-
 import Welcome from '../components/Welcome'
 export default {
 name: 'Cadastro',
--- src/views/Pesquisa.vue
+++ src/views/Pesquisa.vue
@@ -1,20 +1,22 @@
 <template>
-  <div>
-    <div class="container">
-      <div>
+  <div class="container">
+    <div class="row">
+      <div class="col-4">
         <img class="vue-dog" alt="VueDog" src="../assets/vue-dog.jpg">
       </div>
-      <div class="pesquisa-txt">
+      <div class="pesquisa-txt col-4">
         <Welcome msg="Pesquisa de Pet" txt="Encontre um pet pesquisando por nome"></Welcome>
-      </div>
-      <div class="div-input ">
+        <div class="div-input">
         <form>
-          <div class="form-group">
-                <label for="nome">Insira o nome do pet</label>
-                <input type="nome" class="form-control" id="raça"/>
+          <div>
+            <label for="nome">Insira o nome do pet</label>
+            <input type="nome" class="form-control" id="raça"/>
           </div>
-          <button type="submit" class="btn-input btn">Pesquisar</button>
         </form>
+      </div>
+      </div>
+      <div class="col-4">
+            <button @click="getListaCachorros" type="button" class="btn-input btn">Pesquisar</button>
       </div>
     </div> 
   </div>
@@ -28,5 +30,49 @@ export default {
     components: {
     Welcome,
     },
+    data() {
+    return {
+      cachorros: {},
+      resultadoOperacaoApi: [],
+    };
+    },
+    mounted() {
+    fetch("http://localhost:8080/cachorros", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok === true) {
+          return response.json();
+        }
+      })
+      .then((cachorros) => {
+        this.cachorros = cachorros;
+      });
+  },
+    methods: {
+      getListaCachorros() {
+        fetch(
+          "http://localhost:8080/cachorros", 
+          {
+            method: "GET",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((resposta) => {
+         if (resposta.ok) return resposta.json();
+        })
+
+        .then((jsonCachorros) => {
+          this.resultadoOperacaoApi = jsonCachorros;
+          console.log(jsonCachorros);
+        })
+      }
+    }
 }
 </script>
